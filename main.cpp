0bc29152f0dac1da21e881e551d7b5373d751666
juanFelipe2205 <153253730+juanFelipe2205@users.noreply.github.com>
2024-04-08 17:11:44 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "matriz_operaciones.h"
+#include "cerraduras.h"
+
+using namespace std;
+
+int main() {
+    const int clave[] = {4, 3, 1, -1, 1};
+    GenerarCerraduras(clave);
+
+    return 0;
+}
