f0f8c70e5ad31d1946887524a37dcfeea40255aa
juanFelipe2205 <153253730+juanFelipe2205@users.noreply.github.com>
2024-04-08 17:05:18 -0500

    Create matriz_operaciones.h

---
+++ matriz_operaciones.h
@@ -0,0 +1,10 @@
+#ifndef MATRIZ_OPERACIONES_H
+#define MATRIZ_OPERACIONES_H
+
+const int MAX_SIZE = 15;
+
+void generarMatriz(int matriz[MAX_SIZE][MAX_SIZE], int tamaño);
+
+void rotarMatrizContraManecillas(int matriz[MAX_SIZE][MAX_SIZE], int tamaño);
+
+#endif
